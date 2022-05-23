350d76853f98943e9155d3238fe24be68d28e41e
Anuvrat Karanwal <anuvratkaranwal@Apples-MacBook-Pro-10.local>
2022-05-23 14:14:51 +0530

    updates test cases and updated the logic accordingly for the failed test cases

--- anagram-checker/src/AnagramChecker.kt
+++ anagram-checker/src/AnagramChecker.kt
@@ -1,10 +1,18 @@
 class AnagramChecker {
     fun match(word1: String, word2: String): Boolean {
+          var str1 = word1.lowercase()
+          var str2 = word2.lowercase()
         val counter = IntArray(26) { 0 }
-        if (word1.length != word2.length) return false
+//        if (word1.length != word2.length) return false
+//        (word1.indices).forEach { index ->
+//            ++counter[word1[index] - 'a']
+//            --counter[word2[index] - 'a']
+//        }
+
+        if (str1.length != word2.length) return false
         (word1.indices).forEach { index ->
-            ++counter[word1[index] - 'a']
-            --counter[word2[index] - 'a']
+            ++counter[str1[index] - 'a']
+            --counter[str2[index] - 'a']
         }
         return !counter.any { it != 0 }
     }
--- anagram-checker/test/AnagramCheckerTest.kt
+++ anagram-checker/test/AnagramCheckerTest.kt
@@ -1,10 +1,32 @@
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 
 class AnagramCheckerTest : StringSpec() {
     init {
-        "is 'a' an anagram of 'a'" {
+        "is lowercase 'a' an anagram of lowercase 'a'" {
             AnagramChecker().match("a", "a").shouldBeTrue()
         }
+
+        "is an uppercase 'A' an anagram of lowercase 'a'" {
+            AnagramChecker().match("A", "a").shouldBeTrue()
+        }
+
+        "is an uppercase 'A' an anagram of other uppercase 'A'" {
+            AnagramChecker().match("A", "A").shouldBeTrue()
+        }
+
+        "is single character 'a' an anagram to a string of 'aa'" {
+            AnagramChecker().match("a", "aa").shouldBeFalse()
+        }
+
+        "is string 'abc' an anagram to any of its permutation string 'cab'" {
+            AnagramChecker().match("abc", "cab").shouldBeTrue()
+        }
+
+        "is string 'a' an anagram to a different string with same number of characters 'b'" {
+            AnagramChecker().match("a", "b").shouldBeFalse()
+        }
+
     }
 }
